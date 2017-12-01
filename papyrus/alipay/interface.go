bbb8e077f1e0f1ccfb75152b2bf608707ae0309c
timeloveboy <734991033@qq.com>
2017-12-01 17:17:27 +0800

    ~

--- papyrus/alipay/interface.go
+++
@@ -1,8 +0,0 @@
-package alipay
-
-type AliPayer interface {
-	ChooseApi() string
-	SetExtraParam() (isNil bool, m map[string]string)
-	BizContentKey() (isNil bool, key string)
-	BizContentValue() string
-}
